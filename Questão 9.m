1acb03742a3db417cb0bc47fdb18932dd27145a6
Maria Gabriele <153842959+mgmeirellis@users.noreply.github.com>
2024-03-18 09:39:22 -0400

    Questão 9.m

---
+++ Questão 9.m
@@ -0,0 +1,18 @@
+Verificador de Maioridade
+
+Objetivo: Ler a idade de uma pessoa e verificar se ela é maior de 18 anos.
+
+programa {
+  funcao inicio() {
+    inteiro idade
+
+    escreva("Digite sua idade: ")
+    leia(idade)
+
+    se (idade>=18){
+      escreva("Você é maior de idade")
+    } senao {
+      escreva("Você é menor de idade")
+    }
+  }
+}
